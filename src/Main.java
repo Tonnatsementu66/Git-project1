0106a3069befaf50768adba346502857e2a68800
tonnatsementu66 <tonnatsementu@gmail.com>
2024-08-11 12:42:51 -0400

    third

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Syntax");
         System.out.println("Hello Batch20");
+        System.out.println("Hello Syntax");
+        System.out.println("Hello Batch20");
+        System.out.println("Hello world!");
+
 
 
 
